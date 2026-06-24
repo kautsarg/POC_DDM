03229b20b85e05db3ebd95b1716f1337bd147f0e
Gifari Kautsar <gifari.kautsar25@imperial.ac.uk>
2026-06-24 15:49:29 +0100

    misc codes

--- slurm_jobs/multi_full_pipeline.sh
+++ slurm_jobs/multi_full_pipeline.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
-#SBATCH --partition=a30
+#SBATCH --partition=a40
 #SBATCH --array=2
 
 # Output and Error logs (using SLURM variables to prevent overwriting)
@@ -42,8 +42,11 @@ EXP_FOLDER=/vol/bitbucket/gk225/POC_DDM_datasets/POC_DDM_multi
 # done
 
 TRAIN_FOLDER="${EXP_FOLDER}_nc_subtract"
+
+python -u /vol/bitbucket/gk225/POC_DDM/gk_code/main/01_curve_preprocessing_v6.py --task_id $REAL_TASK_ID --exp_folder "$EXP_FOLDER" --n_wells 10 --n_a_type v06 --nc_subtract
+python -u /vol/bitbucket/gk225/POC_DDM/gk_code/main/02_outlier_detection_pipeline.py --task_id $REAL_TASK_ID --exp_folder "$TRAIN_FOLDER" --fast_mode
 python -u /vol/bitbucket/gk225/POC_DDM/gk_code/main/03_main_training.py --task_id $REAL_TASK_ID --exp_folder "$TRAIN_FOLDER" --n_splits 1 --fast_mode
 python -u /vol/bitbucket/gk225/POC_DDM/gk_code/main/06_model_prediction_report.py --task_id $REAL_TASK_ID --exp_folder "$TRAIN_FOLDER"
-python -u /vol/bitbucket/gk225/POC_DDM/gk_code/main/07_attribution_vis_all.py --task_id $REAL_TASK_ID --exp_folder "$TRAIN_FOLDER"
+python -u /vol/bitbucket/gk225/POC_DDM/gk_code/main/07_attribution_vis_all.py --task_id $REAL_TASK_ID --exp_folder "$TRAIN_FOLDER" 
 
 deactivate
